b08872779b0b963d637b2f32408f82210d553a8c
wangguojing <996340021@qq.com>
2018-03-23 15:14:44 +0800

    库表的类型支持set, bag两种类型

--- src/db.erl
+++ src/db.erl
@@ -12,10 +12,11 @@
 -include_lib("eunit/include/eunit.hrl").
 
 %% API
--export([insert/3, read/2, delete/2, test/0, start/1, db1/1, test1/0,test2/0]).
+%%-export([insert/3, read/2, delete/2, test/0, start/1, db1/1, test1/0,test2/0]).
+-compile(export_all).
 
-start(Table) ->
-    Pid=spawn(?MODULE, db1, [[]]),
+start(Table,TableType) ->
+    Pid=spawn(?MODULE, db1, [[],TableType]),
     io:format("start function--- Table: ~p,Pid:~p~n",[Table,Pid]),
     register(Table, Pid).
 
@@ -59,24 +60,24 @@ delete(Table, Key) ->
     end.
 
 
-db1(Res) ->
+db1(Res,TableType) ->
     receive
         {From, Ref, {insert, {Key, Val}}} ->
             io:format("db1 function--- Key: ~p,Val: ~p, Res:~p~n",[Key,Val,Res]),
-            case insert_db(Key, Val, Res) of
+            case insert_db(TableType,Key, Val, Res) of
                 false ->
                     From ! {Ref, false},
-                    db1(Res);
+                    db1(Res,TableType);
                 Res1 ->
                     From ! {Ref, ok},
-                    db1(Res1)
+                    db1(Res1,TableType)
             end;
         {From, Ref, {read, Key}} ->
-            From ! {Ref, read_db(Key, Res)},
-            db1(Res);
+            From ! {Ref, read_db(TableType,Key, Res)},
+            db1(Res,TableType);
         {From, Ref, {delete, Key}} ->
             From ! {Ref, ok},
-            db1(delete_db(Key, Res));
+            db1(delete_db(TableType,Key, Res),TableType);
         terminate ->
             ok
     end.
@@ -85,31 +86,43 @@ db1(Res) ->
 
 
 
-insert_db([], _, _) ->
-    false;
-insert_db('', _, _) ->
-    false;
-insert_db(<<"">>, _, _) ->
-    false;
-insert_db(Key, Val, []) ->
-    [{Key, Val}];
-insert_db(Key, Element, Db) ->
-    case read_db(Key, Db) of
-        [] -> [{Key, Element} | Db];
-        {Key, _} -> update_db(Key, Element, Db)
+
+
+insert_db(_,Key,Val,[])->
+    [{Key,Val}];
+insert_db(set,Key,Val,DB)->
+    case read_db(set,Key, DB) of
+        [] -> [{Key, Val} | DB];
+        [{Key, _}] -> update_db(Key, Val, DB)
+    end;
+insert_db(bag,Key,Val,DB)->
+    case find(Key,Val, DB) of
+        false -> [{Key, Val} | DB];
+        true -> DB
     end.
 
+find(_,_,[])->
+    false;
+find(Key,Val,[{Key,Val}|_])->
+    true;
+find(Key,Val,[_|Rest])->
+    find(Key,Val,Rest).
 
-read_db(_, []) -> [];
-read_db(Key, [{Key, Val} | _]) ->
-    {Key, Val};
-read_db(Key, [_H | Rest]) ->
-    read_db(Key, Rest).
 
-delete_db(Key, [{Key, _} | Rest]) -> Rest;
-delete_db(Key, [H | Rest]) ->
-    [H | delete_db(Key, Rest)];
-delete_db(_, []) -> [].
+read_db(_,_, []) -> [];
+read_db(set,Key, [{Key, Val} | _]) ->
+    [{Key, Val}];
+read_db(bag,Key,[{Key,Val}|Rest])->
+    [{Key, Val}|read_db(bag,Key,Rest)];
+read_db(TableType,Key, [_H | Rest]) ->
+    read_db(TableType,Key, Rest).
+
+delete_db(set,Key, [{Key, _} | Rest]) -> Rest;
+delete_db(bag,Key,[{Key,_}|Rest])->
+    delete_db(bag,Key,Rest);
+delete_db(TableType,Key, [H | Rest]) ->
+    [H | delete_db(TableType,Key, Rest)];
+delete_db(_,_, []) -> [].
 
 update_db(_, _, []) -> [];
 update_db(Key, Val, [{Key, _} | Rest]) ->
@@ -119,21 +132,38 @@ update_db(Key, Val, [H | Rest]) ->
 
 test() ->
     ?assertEqual(true,is_atom(t1)),
-    start(t1),
-    ?assertEqual(false, insert(t1, '', 2)),
-    ?assertEqual(false, insert(t1, [], 2)),
-    ?assertEqual(false, insert(t1, <<"">>, 2)),
+    start(t1,set),
     ?assertEqual(ok, insert(t1, 1, 2)),
-    ?assertEqual({1, 2}, read(t1, 1)),
+    ?assertEqual([{1, 2}], read(t1, 1)),
     ?assertEqual([], read(t1, 2)),
     ?assertEqual(ok, insert(t1, 2, 3)),
-    ?assertEqual({2, 3}, read(t1, 2)),
+    ?assertEqual([{2, 3}], read(t1, 2)),
     ?assertEqual(ok, insert(t1, a, 4)),
     ?assertEqual(ok, delete(t1, 2)),
     ?assertEqual([], read(t1, 2)),
     ?assertEqual(ok, insert(t1, 2, t)),
     ?assertEqual(ok, insert(t1, a, s)),
-    ?assertEqual({a, s}, read(t1, a)).
+    ?assertEqual([{a, s}], read(t1, a)),
+
+    start(t2,bag),
+    ?assertEqual([],read(t2,2)),
+    ?assertEqual(ok,insert(t2,1,2)),
+    ?assertEqual([{1,2}],read(t2,1)),
+    ?assertEqual([],read(t2,2)),
+    ?assertEqual(ok,insert(t2,2,3)),
+    ?assertEqual([{2,3}],read(t2,2)),
+    ?assertEqual(ok,insert(t2,1,4)),
+    ?assertEqual([{1,4},{1,2}],read(t2,1)),
+    ?assertEqual(ok,delete(t2,2)),
+    ?assertEqual([],read(t2,2)),
+    ?assertEqual(ok,insert(t2,a,5)),
+    ?assertEqual(ok,insert(t2,a,5)),
+    ?assertEqual([{a,5}],read(t2,a)),
+    ?assertEqual(ok,insert(t2,a,7)),
+    ?assertEqual(ok,insert(t2,a,6)),
+    ?assertEqual([{a,6},{a,7},{a,5}],read(t2,a)),
+    ?assertEqual(ok,delete(t2,a)),
+    ?assertEqual([],read(t2,a)).
 
 
 test2()->
